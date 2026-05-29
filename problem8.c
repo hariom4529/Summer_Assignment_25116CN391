46645324642b927ea6a69a27da272181573d290e
Hariom Thakur <fhariom4@gmail.com>
2026-05-29 11:47:45 +0530

    Add files via upload

---
+++ problem8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+    int n,rev=0,temp;
+    printf("Enter Number : ");
+    scanf("%d",&n);
+    temp = n;
+    while (temp!=0){
+        rev = rev*10 + temp%10;
+        temp = temp/10;
+    }
+    if (rev == n){
+        printf("The Number Is Palindrome");
+    }
+    else{
+        printf("The Number Is Not Palindrome");
+    }   
+    return 0;
+}
