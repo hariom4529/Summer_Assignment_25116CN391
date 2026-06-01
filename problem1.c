acfe6ef54771e760ac82ef54b0b64f1bf2231487
Hariom Thakur <hariomthakur@Harioms-MacBook-Air.local>
2026-06-01 12:03:53 +0530

    Add Day 1 programs

---
+++ problem1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    printf("Enter Number : ");
+    scanf("%d",&n);
+    printf("Sum of First %d Natural Number is : %d",n,n*(n+1)/2);
+    return 0;
+}
